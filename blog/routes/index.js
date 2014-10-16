9c6378eb3207817b34bd4970bac6dcc396a728c4
foool <foool@foool-PC>
2014-10-17 00:49:59 +0800

    添加权限控制

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -26,6 +26,7 @@ module.exports = function(app){
 		});
 	});
 	
+	app.get('/reg',checkNotLogin);
 	app.get('/reg',function(req,res){
 		res.render('reg',{
 			title : '注册',
@@ -34,6 +35,7 @@ module.exports = function(app){
 			error : req.flash('error').toString()
 		});
 	});
+	app.get('/reg',checkNotLogin);
 	app.post('/reg',function(req,res){
 		var name = req.body.name,
 			password = req.body.password,
@@ -66,7 +68,7 @@ module.exports = function(app){
 			});
 		});
 	});
-	
+	app.get('/login',checkNotLogin);
 	app.get('/login',function(req,res){
 		res.render('login',{
 			title : '登录',
@@ -75,7 +77,25 @@ module.exports = function(app){
 			error : req.flash('error').toString()
 		});
 	});
+	app.get('/login',checkNotLogin);
 	app.post('/login',function(req,res){
+		var md5 = crypto.createHash('md5');
+		var password = req.body.password;
+		password = md5.update(password).digest('hex');
+		var name = req.body.name;
+		User.get(name,function(err,user){
+			if(!user){
+				req.flash('error','用户不存在');
+				res.redirect('/login');
+			}
+			if(user.password !== password){
+				req.flash('error','密码错误');
+				res.redirect('/login');
+			}
+			req.session.user = user;
+			req.flash('success','登陆成功');
+			res.redirect('/');
+		});
 	});
 	
 	app.get('/post',function(req,res){
@@ -86,6 +106,23 @@ module.exports = function(app){
 	});
 	
 	app.get('/logout',function(req,res){
+		req.session.user = null;
+		req.flash('success','登出成功');
+		res.redirect('/');
 	});
 	
+	function checkLogin(req,res,next){
+		if(!req.session.user){
+			req.flash('error','未登录');
+			res.redirect('/login');
+		}
+		next();
+	}
+	function checkNotLogin(req,res,next){
+		if(req.session.user){
+			req.flash('error','已登录');
+			res.redirect('back');
+		}
+		next();
+	}
 };
