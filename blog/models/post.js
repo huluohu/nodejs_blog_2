e7567c9b3f87a35611bcffa4ac3f2373aaf1fc80
foool <foool@foool-PC>
2014-11-17 00:51:18 +0800

    增加留言板和留言功能。

--- blog/models/post.js
+++ blog/models/post.js
@@ -28,7 +28,8 @@ Post.prototype.save = function(callback){
 			name : this.name,
 			time : time,
 			title : this.title,
-			post : this.post
+			post : this.post,
+			comments : []
 	};
 	mongodb.open(function(err,db){
 		if(err){
@@ -100,6 +101,9 @@ Post.getOne = function(name,day,title,callback){
 					return callback(err);
 				}
 				doc.post = markdown.toHTML(doc.post);
+				doc.comments.forEach(function(comment){
+					comment.content = markdown.toHTML(comment.content);
+				});
 				callback(null,doc);
 			});
 		});
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,4 +1,8 @@
-var crypto = require('crypto'), fs = require('fs'), User = require('../models/user.js'), Post = require('../models/post.js');
+var crypto = require('crypto');
+var fs = require('fs');
+var User = require('../models/user.js');
+var Post = require('../models/post.js');
+var Comment = require('../models/comment.js');
 
 module.exports = function(app) {
 	// app.get('/',function(req,res){
@@ -192,6 +196,7 @@ module.exports = function(app) {
 		});
 	});
 
+	//根据用户名、日期和标题查询一个文章
 	app.get('/u/:name/:day/:title', function(req, res) {
 		Post.getOne(req.params.name, req.params.day, req.params.title,
 				function(err, post) {
@@ -207,6 +212,27 @@ module.exports = function(app) {
 						error : req.flash('error').toString()
 					});
 				});
+	});
+	//为文章添加留言
+	app.post('/u/:name/:day/:title', function(req, res) {
+		var date = new Date();
+		var time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes() );
+		var comment = {
+				name : req.body.name,
+				email : req.body.email,
+				website : req.body.website,
+				time : time,
+				content : req.body.content
+		};
+		var newComment = new Comment(req.params.name,req.params.day,req.params.title,comment);
+		newComment.save(function(err){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('back');
+			}
+			req.flash('success','留言成功');
+			res.redirect('back');
+		});
 	});
 	app.get('/edit/:name/:day/:title', checkLogin);
 	app.get('/edit/:name/:day/:title', function(req, res) {
@@ -265,6 +291,7 @@ module.exports = function(app) {
 			res.redirect('/');
 		});
 	});
+	
 	function checkLogin(req, res, next) {
 		if (!req.session.user) {
 			req.flash('error', '未登录');
