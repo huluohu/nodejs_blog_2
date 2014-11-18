60b7f5b754935db3d5a55a09ff931c3c04601c75
foool <foool@foool-PC>
2014-11-19 01:15:05 +0800

    发表文章路由增加文章标签属性

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -147,8 +147,9 @@ module.exports = function(app) {
 
 	app.get('/post', checkLogin);
 	app.post('/post', function(req, res) {
-		var currentUser = req.session.user, post = new Post(currentUser.name,
-				req.body.title, req.body.post);
+		var currentUser = req.session.user;
+		var tags = [req.body.tag1,req.body.tag2,req.body.tag3];
+		var post = new Post(currentUser.name,req.body.title, tags,req.body.post);
 		post.save(function(err) {
 			if (err) {
 				req.flash('error', err);
